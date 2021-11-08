082f46af20f696d34403bc41bdf1d5aa86d1fd17
Andrew <ajbarnes97@gmail.com>
2021-11-08 21:58:14 +1300

    Started Multiplayer Implementation

---
+++ Assets/Scripts/SCR_NetworkManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class SCR_NetworkManager : NetworkManager
+{
+    [SerializeField] private Transform playerOneSpawn;
+    [SerializeField] private Transform playerTwoSpawn;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        // Set player spawn on server connect
+        Transform start = numPlayers == 0 ? playerOneSpawn : playerTwoSpawn;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        //TODO: Destroy the ball for the player that leaves
+        
+        base.OnServerDisconnect(conn);
+    }
+}
